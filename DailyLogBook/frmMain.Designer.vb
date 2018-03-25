24fcf312ab2a49ca335b3ec27327d5e9e7b58838
voonxx <s.hong-5617@hotmail.com>
2018-03-25 17:33:06 +0800

    Added MenuStrip

--- DailyLogBook/frmMain.Designer.vb
+++ DailyLogBook/frmMain.Designer.vb
@@ -24,14 +24,25 @@ Partial Class frmMain
     Private Sub InitializeComponent()
         Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
         Me.FileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
-        Me.HistoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.SaveToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.ExportAsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExitToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.EditToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ViewToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HistoryToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AboutToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
         Me.HelpToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.PNGFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.JPEGFileToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.UserManualToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.DateTimePicker = New System.Windows.Forms.DateTimePicker()
+        Me.MonthCalendar = New System.Windows.Forms.MonthCalendar()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.txtBox1 = New System.Windows.Forms.RichTextBox()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
         Me.MenuStrip1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
         '
         'MenuStrip1
@@ -46,22 +57,35 @@ Partial Class frmMain
         '
         'FileToolStripMenuItem
         '
-        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveToolStripMenuItem, Me.ExportAsToolStripMenuItem})
+        Me.FileToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.SaveToolStripMenuItem, Me.ExportAsToolStripMenuItem, Me.ExitToolStripMenuItem})
         Me.FileToolStripMenuItem.Name = "FileToolStripMenuItem"
         Me.FileToolStripMenuItem.Size = New System.Drawing.Size(44, 24)
         Me.FileToolStripMenuItem.Text = "File"
+        '
+        'SaveToolStripMenuItem
+        '
+        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
+        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(145, 26)
+        Me.SaveToolStripMenuItem.Text = "Save"
+        '
+        'ExportAsToolStripMenuItem
+        '
+        Me.ExportAsToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.PNGFileToolStripMenuItem, Me.JPEGFileToolStripMenuItem})
+        Me.ExportAsToolStripMenuItem.Name = "ExportAsToolStripMenuItem"
+        Me.ExportAsToolStripMenuItem.Size = New System.Drawing.Size(145, 26)
+        Me.ExportAsToolStripMenuItem.Text = "Export as"
+        '
+        'ExitToolStripMenuItem
+        '
+        Me.ExitToolStripMenuItem.Name = "ExitToolStripMenuItem"
+        Me.ExitToolStripMenuItem.Size = New System.Drawing.Size(145, 26)
+        Me.ExitToolStripMenuItem.Text = "Exit"
         '
         'EditToolStripMenuItem
         '
         Me.EditToolStripMenuItem.Name = "EditToolStripMenuItem"
         Me.EditToolStripMenuItem.Size = New System.Drawing.Size(47, 24)
         Me.EditToolStripMenuItem.Text = "Edit"
-        '
-        'AboutToolStripMenuItem
-        '
-        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
-        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
-        Me.AboutToolStripMenuItem.Text = "About"
         '
         'ViewToolStripMenuItem
         '
@@ -73,38 +97,110 @@ Partial Class frmMain
         'HistoryToolStripMenuItem
         '
         Me.HistoryToolStripMenuItem.Name = "HistoryToolStripMenuItem"
-        Me.HistoryToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.HistoryToolStripMenuItem.Size = New System.Drawing.Size(131, 26)
         Me.HistoryToolStripMenuItem.Text = "History"
         '
-        'SaveToolStripMenuItem
+        'AboutToolStripMenuItem
         '
-        Me.SaveToolStripMenuItem.Name = "SaveToolStripMenuItem"
-        Me.SaveToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
-        Me.SaveToolStripMenuItem.Text = "Save"
-        '
-        'ExportAsToolStripMenuItem
-        '
-        Me.ExportAsToolStripMenuItem.Name = "ExportAsToolStripMenuItem"
-        Me.ExportAsToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
-        Me.ExportAsToolStripMenuItem.Text = "Export as"
+        Me.AboutToolStripMenuItem.Name = "AboutToolStripMenuItem"
+        Me.AboutToolStripMenuItem.Size = New System.Drawing.Size(62, 24)
+        Me.AboutToolStripMenuItem.Text = "About"
         '
         'HelpToolStripMenuItem
         '
+        Me.HelpToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.UserManualToolStripMenuItem})
         Me.HelpToolStripMenuItem.Name = "HelpToolStripMenuItem"
         Me.HelpToolStripMenuItem.Size = New System.Drawing.Size(53, 24)
         Me.HelpToolStripMenuItem.Text = "Help"
+        '
+        'PNGFileToolStripMenuItem
+        '
+        Me.PNGFileToolStripMenuItem.Name = "PNGFileToolStripMenuItem"
+        Me.PNGFileToolStripMenuItem.Size = New System.Drawing.Size(142, 26)
+        Me.PNGFileToolStripMenuItem.Text = "PNG File"
+        '
+        'JPEGFileToolStripMenuItem
+        '
+        Me.JPEGFileToolStripMenuItem.Name = "JPEGFileToolStripMenuItem"
+        Me.JPEGFileToolStripMenuItem.Size = New System.Drawing.Size(142, 26)
+        Me.JPEGFileToolStripMenuItem.Text = "JPEG File"
+        '
+        'UserManualToolStripMenuItem
+        '
+        Me.UserManualToolStripMenuItem.Name = "UserManualToolStripMenuItem"
+        Me.UserManualToolStripMenuItem.Size = New System.Drawing.Size(216, 26)
+        Me.UserManualToolStripMenuItem.Text = "User Manual"
+        '
+        'DateTimePicker
+        '
+        Me.DateTimePicker.Location = New System.Drawing.Point(578, 58)
+        Me.DateTimePicker.Name = "DateTimePicker"
+        Me.DateTimePicker.Size = New System.Drawing.Size(200, 22)
+        Me.DateTimePicker.TabIndex = 1
+        '
+        'MonthCalendar
+        '
+        Me.MonthCalendar.Cursor = System.Windows.Forms.Cursors.Default
+        Me.MonthCalendar.Location = New System.Drawing.Point(528, 92)
+        Me.MonthCalendar.Name = "MonthCalendar"
+        Me.MonthCalendar.TabIndex = 2
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.ImageScalingSize = New System.Drawing.Size(20, 20)
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripButton2, Me.ToolStripButton1})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 28)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(800, 27)
+        Me.ToolStrip1.TabIndex = 3
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'txtBox1
+        '
+        Me.txtBox1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtBox1.Location = New System.Drawing.Point(130, 92)
+        Me.txtBox1.Name = "txtBox1"
+        Me.txtBox1.Size = New System.Drawing.Size(360, 257)
+        Me.txtBox1.TabIndex = 4
+        Me.txtBox1.Text = "Record Your Daily Accomplishments "
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton2.Image = Global.DailyLogBook.My.Resources.Resources.Save
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(24, 24)
+        Me.ToolStripButton2.Text = "ToolStripButton2"
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Image
+        Me.ToolStripButton1.Image = Global.DailyLogBook.My.Resources.Resources.Open_File
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(24, 24)
+        Me.ToolStripButton1.Text = "ToolStripButton1"
         '
         'frmMain
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.txtBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Controls.Add(Me.MonthCalendar)
+        Me.Controls.Add(Me.DateTimePicker)
         Me.Controls.Add(Me.MenuStrip1)
         Me.MainMenuStrip = Me.MenuStrip1
         Me.Name = "frmMain"
         Me.Text = "Daily Log "
         Me.MenuStrip1.ResumeLayout(False)
         Me.MenuStrip1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
@@ -119,4 +215,14 @@ Partial Class frmMain
     Friend WithEvents HistoryToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents AboutToolStripMenuItem As ToolStripMenuItem
     Friend WithEvents HelpToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ExitToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents PNGFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents JPEGFileToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents UserManualToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents DateTimePicker As DateTimePicker
+    Friend WithEvents MonthCalendar As MonthCalendar
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents txtBox1 As RichTextBox
+    Friend WithEvents ToolStripButton1 As ToolStripButton
 End Class
--- DailyLogBook/frmMain.vb
+++ DailyLogBook/frmMain.vb
@@ -1,3 +1,13 @@
 ﻿Public Class frmMain
+    Private Sub ExitToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles ExitToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
 
+    Private Sub DateTimePicker1_ValueChanged(sender As Object, e As EventArgs) Handles DateTimePicker.ValueChanged
+
+    End Sub
+
+    Private Sub HelpToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles HelpToolStripMenuItem.Click
+
+    End Sub
 End Class
